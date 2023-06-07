dfdb28c282c48b57201131c09e9d919a25a1592a
Mr.PAPI <thesnakewitcher@gmail.com>
2023-06-06 22:15:29 -0400

    feat: add prerender

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+// import { sveltekit } from "@sveltejs/kit/vite";
+// import { defineConfig } from "vite";
+//
+// export default defineConfig({
+//     plugins: [sveltekit()],
+//     base: "/AuctionHub-ui/",
+//     // build: {
+//     //   outDir: "./dist",
+//     // },
+// });
 
-export default defineConfig({
-    plugins: [sveltekit()],
-    base: "/AuctionHub-ui/",
-    // build: {
-    //   outDir: "./dist",
-    // },
-});
+
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
+};
