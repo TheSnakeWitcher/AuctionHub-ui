e6c581b787ead8a41f00067907c3ecb9e7be958d
Mr.PAPI <thesnakewitcher@gmail.com>
2023-06-06 21:57:17 -0400

    fix: add base field to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  // base: "https://github.com/TheSnakeWitcher/AuctionHub-ui/",
-  // build: {
-  //   outDir: "./dist",
-  // },
+    plugins: [sveltekit()],
+    base: "/AuctionHub-ui/",
+    // build: {
+    //   outDir: "./dist",
+    // },
 });
